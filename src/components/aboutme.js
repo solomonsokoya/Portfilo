5aaf0c067c9112d5cc6e32b2792bfe50643e61c5
Solomon sokoya <solomonsokoya@ool-ad0202c0.dyn.optonline.net>
2018-06-13 13:58:27 -0400

    Made changes to font style

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -13,7 +13,7 @@ export default class AboutMe extends Component {
         <Header as='h2' icon textAlign='center'>
           <Header.Content>Solomon Sokoya</Header.Content>
         </Header>
-        <Container textAlign='center'> Web developer with a background in Computer Engineering. My enthusiasm in my travel experience led me to General Assembly where I was thoroughly immersed in the field. During my time in General Assembly I acquired front end skills in CSS, React.js and back end skills like JavaScript, Node.js, and Express. I was able to work in a community with peers who continue to push me to become a well-rounded Web Developer. I am looking to bring my experiences to an organization that encourages growth and professional development.</Container>
+        <Container textAlign='center'> Web developer with a background in Computer Engineering. My enthusiasm in my travel experience led me to General Assembly where I was thoroughly immersed in the field. During my time in General Assembly I acquired front-end skills in <span>CSS</span>, <span>React.js</span> and backend skills like <span>JavaScript</span>, <span>Node.js</span>, and <span>Express</span>. I was able to work in a community with peers who continue to push me to become a well-rounded Web Developer. I am looking to bring my experiences to an organization that encourages growth and professional development.</Container>
       </div>
       )
       }
