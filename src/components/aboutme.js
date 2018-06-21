b0179cc0143d6ea141744f7055e68ff81e1cf4fe
Solomon sokoya <solomonsokoya@ool-ad020098.dyn.optonline.net>
2018-06-21 17:35:33 -0400

    Added Css to pdf

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -13,20 +13,18 @@ export default class AboutMe extends Component {
       </Header>
       <Container textAlign='center'>
         Web developer with a background in Computer Engineering. My enthusiasm in my travel experience led me to General Assembly where I was thoroughly immersed in the field. I am proficient in
-        <span>Javascript</span>
+        <span> Javascript </span>
         and acquired front-end skills in
-        <span>CSS3</span>,
-        <span>HTML5</span>,
-        <span>JQuery</span>,
-        <span>React.js</span>
+        <span> CSS3</span>,
+        <span> HTML5</span>,
+        <span> JQuery</span>,
+        <span> React.js </span>
         and back-end skills in
-        <span>Rails</span>,
-        <span>Node.js</span>,
-        <span>
-          Firebase
-        </span>
+        <span> Rails</span>,
+        <span> Node.js </span>,
+        <span>Firebase </span>
         and
-        <span>Express</span>. I was able to work in a community with peers who continue to push me to become a well-rounded Web Developer. I am looking to bring my experiences to an organization that encourages growth and professional development.</Container>
+         <span>Express</span>. I was able to work in a community with peers who continue to push me to become a well-rounded Web Developer. I am looking to bring my experiences to an organization that encourages growth and professional development.</Container>
     </div>)
   }
 }
